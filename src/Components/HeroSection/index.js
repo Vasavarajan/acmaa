db66df785cccf5e45a2eb2a91a432c611a138508
Vasavarajan <rajavasu22297@gmail.com>
2022-01-05 17:06:18 +0530

    home screen added

---
+++ src/Components/HeroSection/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Video from "../../Assets/videos/video1.mp4";
+
+import {
+    HeroContainer,
+    HeroBg,
+    VideoBg,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight,
+} from "./HeroElements";
+
+import { Button } from "../ButtonElements";
+
+function HeroSection() {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    };
+
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Lorem ipsum dolor sit amet</HeroH1>
+                <HeroP>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Aliquid reprehenderit accusantium sint tempora suscipit
+                    debitis.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button
+                        to="signup"
+                        onMouseEnter={onHover}
+                        onMouseLeave={onHover}
+                        primary="true"
+                        dark="true"
+                    >
+                        Contact US{hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    );
+}
+
+export default HeroSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,22 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 import Navbar from "../Components/Navbar";
 import Sidebar from "../Components/Sidebar";
+import HeroSection from "../Components/HeroSection";
+
 
 function Home() {
-  const [isOpen,setIsOpen] =useState(false)
+    const [isOpen, setIsOpen] = useState(false);
 
-const toggle=()=>{
-  setIsOpen(!isOpen)
-}
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <>
-            <Sidebar isOpen={isOpen} toggle={toggle}/>
-            <Navbar toggle={toggle}/>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />
+            <HeroSection />
         </>
     );
 }
